93e24507818013228457ae43825fb46bc4c99759
Rodrigodcs <rodrigodcsee@gmail.com>
2023-07-12 10:38:04 -0300

    Code review

--- src/routers/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./users-router";
-export * from "./authentication-router";
-export * from "./posts-router";
-export * from "./comments-router";
--- src/schemas/users-schemas.ts
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-import { CreateUserParams } from "@/utils/protocols";
-
-export const createUserSchema = Joi.object<CreateUserParams>({
-  username: Joi.string().min(3).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
